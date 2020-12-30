a7ad485dcd7758d0df26c5e90160e3ca97976045
Rafael Peria de Sene <rpsene@br.ibm.com>
2020-12-30 13:33:49 -0300

    Usability improvements
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ function  create_additional_volume () {
 
     VOLUME_ID=$(cat ./volume.log | jq -r ".volumeID")
     echo
-    echo "A new volume called $VNAME with $VSIZE G was created with ID $VOLUME_ID."
+    echo "  $VNAME with $VSIZE G was created with ID $VOLUME_ID"
 
     echo "VOLUME_ID=$VOLUME_ID" >> ./server-build.log
     echo "VOLUME_NAME=$VNAME" >> ./server-build.log
@@ -76,14 +76,15 @@ function create_nfs_server () {
     NFS_SERVER_ID=$(cat ./server.log | jq -r ".[].pvmInstanceID")
     NFS_SERVER_NAME=$(cat ./server.log | jq -r ".[].serverName")
 
-    echo "A new server called $NFS_SERVER_NAME was created with the ID $NFS_SERVER_ID."
-    echo
+    echo "  $NFS_SERVER_NAME was created with the ID $NFS_SERVER_ID"
 
     echo "NFS_SERVER_ID=$NFS_SERVER_ID" >> ./server-build.log
     echo "NFS_SERVER_NAME=$NFS_SERVER_NAME" >> ./server-build.log
 
-    echo "Deploying a new VM, please wait."
+    echo "  deploying the server $NFS_SERVER_NAME, hold on please."
     STATUS=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r ".status")
+
+    printf "%c" "    "
     while [[ "$STATUS" != "ACTIVE" ]]
     do
         sleep 5s
@@ -98,47 +99,60 @@ function create_nfs_server () {
     fi
 
     if [[ "$STATUS" == "ACTIVE" ]]; then
-        echo "$NFS_SERVER_NAME is now ACTIVE."
-        echo "Waiting for the network availability..."
-        sleep 25s
+        echo
+        echo "  $NFS_SERVER_NAME is now ACTIVE."
+        echo "  waiting for the network availability, hold on please."
+
         EXTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].externalIP')
-        INTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].ip')
+        printf "%c" "    "
+        while [[ -z "$EXTERNAL_IP" ]]; do 
+            printf "%c" "."
+            EXTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].externalIP')
+            INTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].ip')
+            sleep 5s
+        done
+
         echo "SERVER_EXTERNAL_IP=$EXTERNAL_IP" >> ./server-build.log
+        echo "SERVER_INTERNAL_IP=$INTERNAL_IP" >> ./server-build.log
     fi
+    printf "%c" "    "
     while ! ping -c 1 $EXTERNAL_IP &> /dev/null
     do
         sleep 10s
         printf "%c" "."
     done
-    printf "\n%s\n"  "$NFS_SERVER_NAME is online"
+    echo
+    echo "  copying the NFS builder script..."
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ../../scripts/create-nfs.sh root@$EXTERNAL_IP:
 
-    echo "Copying the NFS builder script..."
-    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ../scripts/create-nfs.sh root@$EXTERNAL_IP:
+    echo "  $NFS_SERVER_NAME is ready, access it using ssh root@$EXTERNAL_IP."
 }
 
 function run (){
 
+    echo "*****************************************************"
     SERVER_ID=$(openssl rand -hex 5)
     SERVER_NAME="nfs-server-$SERVER_ID"
 
-    mkdir -p ./"$SERVER_NAME"
-    cd ./$SERVER_NAME
+    mkdir -p ./servers/"$SERVER_NAME"
+    cd ./servers/$SERVER_NAME
 
     ### Set this variables accordingly
-    VOLUME_SIZE=
-    SERVER_IMAGE=
-    PRIVATE_NETWORK=
-    PUBLIC_NETWORK=
-    SSH_KEY_NAME=
-    SERVER_MEMORY=
-    SERVER_PROCESSOR=
-    SERVER_SYS_TYPE=
+    VOLUME_SIZE=200
+    SERVER_IMAGE=CentOS83-12142020
+    PRIVATE_NETWORK=03c86ee8-a323-4deb-97fe-856e092a4944
+    PUBLIC_NETWORK=77aad4d9-9483-49d9-ab48-ed025b8b970c
+    SSH_KEY_NAME=rpsene
+    SERVER_MEMORY=4
+    SERVER_PROCESSOR=1
+    SERVER_SYS_TYPE=s922
     ####
 
     check_dependencies
     check_connectivity
     create_additional_volume $SERVER_ID $VOLUME_SIZE
     create_nfs_server $SERVER_ID $SERVER_IMAGE $PRIVATE_NETWORK $PUBLIC_NETWORK $SSH_KEY_NAME $SERVER_MEMORY $SERVER_PROCESSOR $SERVER_SYS_TYPE
+    echo "*****************************************************"
 }
 
 ### Main Execution ###
